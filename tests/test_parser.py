70f990f57ccaef265ceefa615b8d15ac344206f7
mux <muxxum65536@gmail.com>
2025-07-01 19:09:28 +0900

    bug fix

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -291,7 +291,7 @@ def test_parse_class_with_access_specifiers():
 
     struct_def = ast.statements[0]
     assert isinstance(struct_def, ClassDef)
-    assert len(struct_def.body.statements) == 2
+    assert len(struct_def.body.statements) == 3
     assert all(isinstance(s, LetStmt) for s in struct_def.body.statements)
 
 
