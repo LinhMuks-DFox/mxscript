0419d84436993d80b4ccf2b3547ae4512c5d331f
mux <muxxum65536@gmail.com>
2025-07-03 12:57:49 +0900

    bugfix

--- runtime/impl/functions.cpp
+++ runtime/impl/functions.cpp
@@ -9,25 +9,25 @@
 #include <iostream>
 #include <ostream>
 #if __has_include(<print>)
-#  include <print>
+#include <print>
 #else
-#  include <format>
-#  include <cstdio>
+#include <cstdio>
+#include <format>
 namespace std {
-    template <class... Args>
-    void print(std::format_string<Args...> fmt, Args&&... args) {
+    template<class... Args>
+    void print(std::format_string<Args...> fmt, Args &&...args) {
         std::fputs(std::format(fmt, std::forward<Args>(args)...).c_str(), stdout);
     }
 }
 #endif
 
 namespace mxs_runtime {
-    auto type_of(const MXObject* obj) -> inner_string { return obj->get_type_name(); }
-    auto type_of(const MXObject& obj) -> inner_string { return obj.get_type_name(); }
+    auto type_of(const MXObject *obj) -> inner_string { return obj->get_type_name(); }
+    auto type_of(const MXObject &obj) -> inner_string { return obj.get_type_name(); }
 }
 
-extern "C" auto mxs_print_object(mxs_runtime::MXObject* obj) -> std::size_t {
+extern "C" auto mxs_print_object(mxs_runtime::MXObject *obj) -> std::size_t {
     auto text = obj->repr();
-    std::print("{}\n", text);
+    std::print("{}", text);
     return text.length();
 }
