34e0afa7a42273b7313903272e2d244eca2893e3
LinhMuks-DFox <muxxum65536@gmail.com>
2025-07-03 16:35:34 +0900

    fix runtime c api duplicates

--- runtime/include/typeinfo.h
+++ runtime/include/typeinfo.h
@@ -16,18 +16,22 @@ namespace mxs_runtime {
         MXObject *(*op_sub)(MXObject *, MXObject *);
         MXObject *(*op_eq)(MXObject *, MXObject *);
     };
-
-    extern "C" {
-    MXS_API mxs_runtime::MXObject *integer_add_integer(mxs_runtime::MXObject *,
-                                                       mxs_runtime::MXObject *);
-    MXS_API mxs_runtime::MXObject *integer_sub_integer(mxs_runtime::MXObject *,
-                                                       mxs_runtime::MXObject *);
-    MXS_API mxs_runtime::MXObject *mxs_op_add(mxs_runtime::MXObject *,
-                                              mxs_runtime::MXObject *);
-    MXS_API mxs_runtime::MXObject *mxs_op_sub(mxs_runtime::MXObject *,
-                                              mxs_runtime::MXObject *);
-    MXS_API mxs_runtime::inner_integer mxs_get_integer_value(mxs_runtime::MXObject *);
-    }
 }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+MXS_API mxs_runtime::MXObject *integer_add_integer(mxs_runtime::MXObject *,
+                                                   mxs_runtime::MXObject *);
+MXS_API mxs_runtime::MXObject *integer_sub_integer(mxs_runtime::MXObject *,
+                                                   mxs_runtime::MXObject *);
+MXS_API mxs_runtime::MXObject *mxs_op_add(mxs_runtime::MXObject *,
+                                          mxs_runtime::MXObject *);
+MXS_API mxs_runtime::MXObject *mxs_op_sub(mxs_runtime::MXObject *,
+                                          mxs_runtime::MXObject *);
+MXS_API mxs_runtime::inner_integer mxs_get_integer_value(mxs_runtime::MXObject *);
+#ifdef __cplusplus
+}
+#endif
 
 #endif// MXS_TYPEINFO_H
