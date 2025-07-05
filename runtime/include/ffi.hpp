edec4fd7f21b2141ef7f6c6e99eb8a27058e3be8
LinhMuks-DFox <muxxum65536@gmail.com>
2025-07-05 14:36:41 +0900

    Refactor FFI dispatch

--- runtime/include/ffi.hpp
+++ runtime/include/ffi.hpp
@@ -5,12 +5,12 @@
 #include "macro.hpp"
 #include "object.h"
 
-namespace mxs_runtime { }
+namespace mxs_runtime {
+    class MXObject;
+    MXS_API MXObject *mxs_ffi_call(MXObject *lib, MXObject *name, MXObject *argv);
+}
 
 extern "C" {
-MXS_API mxs_runtime::MXObject *mxs_ffi_call(mxs_runtime::MXObject *lib_name_obj,
-                                            mxs_runtime::MXObject *func_name_obj,
-                                            int argc, mxs_runtime::MXObject **argv);
 MXS_API mxs_runtime::MXObject *mxs_variadic_print(mxs_runtime::MXObject *fmt,
                                                   mxs_runtime::MXObject *args);
 }
